b2fc15927e0ef766646825761c50e4d7a52adaed
Kazi Md Irshad <56939563+irri094@users.noreply.github.com>
2023-05-15 04:16:22 +0600

    Create script.js

---
+++ may15/script.js
@@ -0,0 +1,96 @@
+start_time = JSON.parse('1675306800.0')
+problems = JSON.parse('["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"]')
+results = JSON.parse('{'DU_NotStrongEnough University of Dhaka': [(108, 0), (29, 0), None, (16, 0), None, (205, 0), (36, 0), (92, 0), (57, 0), (142, 0)], 'Three of a Kind Indian Institute of Technology – Madras': [(61, 0), (94, 1), None, (16, 0), None, (163, 0), (22, 0), (245, 0), (50, 0), (141, 0)], 'BUET Potatoes Bangladesh University of Engineering and Technology': [(69, 0), (125, 1), None, (42, 2), (42, 2), (263, 1), (11, 0), (96, 0), (54, 0), (208, 1)], 'FLogic International Institute of Information Technology – Hyderabad': [(112, 1), (46, 0), None, (21, 0), None, (272, 3), (43, 0), (196, 0), (70, 1), (181, 1)], 'RUET_Aftermath Rajshahi University of Engineering & Technology': [(162, 0), (72, 2), None, (14, 0), None, (267, 0), (24, 0), (284, 0), (101, 0), (130, 0)], 'Kaala Khatta Indian Institute of Technology – Roorkee': [(89, 0), (160, 2), None, (11, 0), None, (187, 1), (31, 0), (245, 0), (141, 0), (241, 0)], 'seize_means_of_software_production Birla Institute of Technology and Science, Pilani Campus': [(119, 1), (65, 3), None, (14, 0), (14, 0), (148, 0), (27, 0), None, (74, 0), (198, 0)], 'AlphaZer0 IIT Bhubaneswar': [(123, 1), (94, 1), None, (15, 1), None, (161, 1), (18, 0), (194, 1), (72, 0), None], 'strawberryparfait Indian Institute of Technology – Madras': [(95, 0), (134, 2), None, (29, 0), (29, 0), (207, 0), (12, 0), (161, 2), (139, 0), None], 'rng(weed) Indian Institute of Technology – Varanasi': [(92, 0), (134, 3), None, (12, 0), None, (12, 0), (31, 0), (128, 1), (157, 0), (243, 0)], 'Convex Chull Indian Institute of Technology (ISM), Dhanbad': [(179, 3), (82, 0), None, (28, 0), None, (256, 0), (18, 0), (227, 0), (64, 0), (64, 0)], 'Triumvirate73 Indian Institute of Technology – Patna': [(228, 1), (48, 0), None, (29, 1), None, (278, 0), (17, 0), None, (169, 0), (118, 0)], '2Golu1Tonu National Institute of Technology, Trichy': [(189, 2), (93, 1), None, (33, 0), None, (33, 0), (42, 0), (204, 2), (155, 0), (256, 4)], 'PGP IIIT-Delhi': [(204, 4), (138, 2), None, (16, 0), None, (285, 2), (21, 1), None, (121, 0), (221, 0)], 'AuditFail Indian Institute of Technology – Delhi': [(260, 7), (131, 2), None, (24, 1), None, (292, 1), (38, 0), (38, 0), (109, 0), (175, 0)], 'PalindORme BITS-Pilani, Hyderabad Campus': [(114, 0), (39, 0), None, (13, 1), None, (269, 1), (20, 0), (20, 0), (74, 0), (74, 0)], 'JU_kzvd4729 Jahangirnagar University': [(74, 0), (65, 2), None, (19, 0), None, (19, 0), (28, 0), (172, 2), (102, 0), (284, 0)], 'dpbois Indian Institute of Technology – Roorkee': [(153, 1), (187, 1), None, (22, 0), None, (258, 0), (29, 0), None, (94, 0), None], 'SIMLEX_BruteForce Indian Institute of Technology – Kanpur': [(215, 4), (46, 0), None, (33, 2), None, None, (29, 0), None, (109, 0), (290, 0)], 'AIUB_Captured American International University – Bangladesh': [(136, 0), (82, 0), None, (35, 1), None, None, (29, 0), None, (243, 2), (294, 2)], 'ladaiLadai International Institute of Information Technology – Hyderabad': [(202, 0), (168, 2), None, (30, 2), None, (226, 0), (48, 0), None, (144, 3), (144, 3)], 'Another_unknown_team Indian Institute of Information Technology Pune': [(221, 3), (133, 0), None, (22, 0), None, None, (28, 0), (28, 0), (196, 0), (263, 2)], 'Khauff Indian Institute of Technology – Delhi': [(162, 0), (104, 2), None, (23, 0), None, (255, 0), (131, 3), None, (182, 1), None], 'Seirin Sri Sivasubramaniya Nadar College of Engineering': [(105, 1), (69, 0), None, (8, 0), None, None, (18, 0), None, (146, 0), (146, 0)], 'CUET_Sapphire Chittagong University of Engineering & Technology': [(146, 0), (56, 0), None, (16, 0), None, None, (22, 0), None, (116, 0), (245, 2)], 'Ujjwal ke bharose Indian Institute of Technology – Mandi': [(203, 0), (86, 0), None, (41, 0), None, None, (50, 0), None, (148, 1), (148, 1)], 'PastGlory PDPM Indian Institute of Information Technology Design & Manufacturing, Jabalpur': [None, (95, 2), None, (23, 0), None, None, (33, 0), (233, 1), (141, 0), (141, 0)], 'KUET_Effervescent Khulna University of Engineering & Technology': [(222, 2), (131, 2), None, (12, 0), None, (12, 0), (23, 0), None, (187, 0), (187, 0)], 'SUST_Carnage Shahjalal University of Science and Technology': [(194, 1), (102, 1), None, (17, 0), None, (250, 1), (38, 0), None, (38, 0), (38, 0)], 'Runtime Terrors Indian Institute of Technology – Bombay': [(184, 1), (165, 0), None, (37, 0), None, None, (57, 1), None, None, (264, 0)], 'Paradigm Shift Indian Institute of Technology – Indore': [(202, 4), (189, 3), None, (29, 0), None, None, (53, 0), None, (194, 1), None], 'Pratyahara Indian Institute Of Technology Jodhpur': [(248, 0), (168, 2), None, (23, 0), None, None, (29, 0), None, (294, 2), None], 'RuntimeEXception Punjab Engineering College Chandigarh': [(294, 2), (93, 0), None, (21, 0), None, None, (30, 0), None, (30, 0), (53, 1)], 'Tree Idiots Institute of Engineering and Technology – Devi Ahilya Univ.': [(53, 1), (68, 0), None, (23, 0), None, (23, 0), (32, 0), None, (155, 0), None], 'RU_404 University of Rajshahi': [(155, 0), (102, 0), None, (27, 0), None, (156, 0), (37, 0), None, None, (37, 0)], 'Kobi Kende Fellen Academy of Technology, Hooghly': [(37, 0), (93, 0), None, (14, 0), None, None, (29, 1), None, (216, 0), None], 'Team_Robust University of Barisal': [(246, 2), (60, 1), None, (15, 0), None, None, (25, 0), None, (25, 0), None], 'JnU-The_Last_Phase Jagannath University, Bangladesh': [(25, 0), (67, 0), None, (38, 0), None, None, (30, 0), None, (30, 0), (252, 1)], 'HSTU_TREENITY Hajee Mohammad Danesh Science and Technology University': [None, (148, 1), None, (15, 0), None, None, (26, 0), None, (270, 1), None], 'AlgoManiaX BITS-Pilani, Goa Campus': [(270, 1), (98, 2), None, (20, 0), None, None, (36, 0), None, (288, 1), None], 'Dunno Indian Institute of Technology – Delhi': [(216, 2), (182, 3), None, (21, 0), None, None, (12, 0), None, (12, 0), None], 'IUT_Reversehash Islamic University of Technology': [(12, 0), (184, 1), None, (41, 3), None, None, (62, 2), None, (258, 1), None], 'Team Blast Indian Institute of Information Technology,Una': [(258, 1), (101, 0), None, (30, 0), None, None, (47, 0), None, (47, 0), None], 'respawn Delhi Technological University(Formerly DCE)': [None, (151, 1), None, (9, 0), None, None, (20, 0), None, (20, 0), None], 'AUST_Onslaught Ahsanullah University of Science and Technology': [(20, 0), (167, 2), None, (24, 0), None, None, (14, 0), None, (14, 0), None], 'CoU_Unpredictable_3207 Comilla University': [(14, 0), (161, 2), None, (22, 0), None, None, (38, 0), None, (38, 0), None], 'MBSTU_Durbeen Mawlana Bhashani Science & Technology University': [(38, 0), (167, 2), None, (23, 0), None, None, (53, 1), None, (53, 1), None], 'Pentateuch Ghulam Ishaq Khan Institute of Engineering Sciences and Technology': [(53, 1), (158, 2), None, (42, 0), None, None, (25, 2), None, None, None], 'BSMRSTU_DreamsOfSuccess Bangabandhu Sheikh Mujibur Rahman Science and Technology University': [None, (184, 3), None, (56, 0), None, None, (31, 0), None, (31, 0), None], 'Trojans 1 NU-FAST Karachi': [(31, 0), (126, 0), None, (82, 3), None, None, (72, 0), None, None, None], 'Zero^Infinity Dhaka University of Engineering & Technology – Gazipur': [None, (247, 1), None, (26, 0), None, None, (51, 0), None, None, None], 'IST_neverGiveUp Institute of Science and Technology': [(51, 0), (184, 5), None, (21, 0), None, None, (78, 1), None, (78, 1), None], 'PSTU_Renaissance Patuakhali Science and Technology University': [None, (255, 5), None, (10, 0), None, None, (47, 1), None, (47, 1), None], 'DIU_Depuradores Daffodil International University': [None, (47, 1), None, (14, 0), None, None, (31, 0), None, (31, 0), None], 'OR’1’=’1′;–– Amrita School of Engineering, Coimbatore': [(31, 0), None, None, (17, 0), None, None, (28, 0), None, (28, 0), None], 'MIST_FrostByte Military Institute of Science and Technology': [(28, 0), (28, 0), None, (27, 0), None, None, (62, 1), None, (62, 1), None], 'SignBiT National University of Computer and Emerging Sciences': [None, (62, 1), None, (74, 0), None, None, (74, 0), None, None, None], 'bitfocus Test Institute 1': [None, (74, 0), None, (74, 0), None, None, (74, 0), None, None, None]}')
+counts = []
+scores = []
+for(var i = 0; i<problems.length; i++){
+    counts.push(0)
+}
+
+cur_time = Date.now()/1000
+
+var remaining
+if (cur_time - start_time > 5 * 60 * 60) {
+    remaining = "Finished"
+}
+else{
+    elapsed_time = cur_time - start_time
+    hours = Math.floor((5 * 60 * 60 - elapsed_time)/60/60)
+    minutes = Math.floor((5 * 60 * 60 - elapsed_time)/60)  % 60
+    seconds = Math.floor((5 * 60 * 60 - elapsed_time))  % 60
+    remaining = `Remaining: ${pad(hours)}:${pad(minutes)}:${pad(seconds)}`
+}
+
+elapsed_time = (cur_time - start_time)/60
+
+for(var team in results){
+    var solve_count = 0
+    var penalty = 0
+
+    for(var i = 0; i<results[team].length; i++){
+        if(results[team][i] !== null &&  results[team][i][0] > elapsed_time)
+            results[team][i] = null
+
+        if(results[team][i] !== null){
+            solve_count += 1
+            penalty += results[team][i][0] + 20 * results[team][i][1]
+            counts[i]++
+        }
+    }
+
+    scores.push([solve_count, penalty, team])
+}
+
+scores = scores.sort((a, b) => {
+    if(a[0] == b[0])
+        return a[1] - b[1]
+    else
+        return b[0] - a[0]
+})
+
+
+function pad(x){
+    y = x.toString()
+    if (y.length < 2)
+        return '0' + y
+    else
+        return y
+}
+
+function update(){
+    document.querySelector('.h3').innerText = remaining
+
+    table = document.querySelector('.table-standings')
+
+    problem_row = table.children.item(0).children.item(0)
+    problems.forEach((x) => {
+        problem_row.innerHTML += `<th style="width: 24px;"><a href="">${x}</a></th>`
+    })
+
+    count_row = table.children.item(0).children.item(1)
+    counts.forEach((x) => {
+        count_row.innerHTML += `<th class="text-center">${x}</th>`
+    })
+
+    ranks = table.children.item(1)
+
+    content = ''
+    for(var i = 0 ; i<scores.length; i++){
+        row_head = `<td>${i+1}</td><td><a href="">${scores[i][2]}</td><td><a href=""><div class="label label-info">${scores[i][0]}</div><div class="label label-default">${scores[i][1]}</div></a><td style="width: 24px;"></td></td>`
+        row_tail = ''
+        team = scores[i][2]
+
+        for(var j = 0; j<results[team].length; j++){
+            if(results[team][j] === null){
+                row_tail += `<td><a href=""></a></td>`
+            }
+            else{
+                row_tail += `<td><a href=""><div class="label label-success">&nbsp;</div><div class="label label-default">${results[team][j][1]+1} (${results[team][j][0]})</div></a></td>`
+            }
+        }
+
+        content += `<tr data-id="undefined">${row_head}${row_tail}</tr>`
+    }
+
+    ranks.innerHTML += content
+}
